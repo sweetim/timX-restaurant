ec7748a1e4094fd6d7f2ab27b8bfc7e81b7a2b4c
sweetim <ho@zmp.co.jp>
2016-04-11 17:31:39 +0900

    enable cors for restaurant api

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,14 @@ const express = require('express');
 const router = express.Router();
 const restaurantAPI = require('./restaurant');
 
-router.use('/restaurant', restaurantAPI);
+router.use('/restaurant', enableCors, restaurantAPI);
+
+function enableCors(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+
+    next();
+}
 
 module.exports = router;
